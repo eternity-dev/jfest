74289082fcf63501a1e6ceb717dc717f2bf6985d
eternalbeginner <eternal.beginner.dev@gmail.com>
2023-08-21 22:32:56 +0800

    refactor (migration): modify some tables

---
+++ database/migrations/2023_08_21_134103_modify_competitions_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('competitions', function (Blueprint $table) {
+            $table->boolean('use_instagram_field')->default(false)->after('image_url');
+            $table->boolean('use_nickname_field')->default(false)->after('use_instagram_field');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('competitions', function (Blueprint $table) {
+            $table->dropColumn([
+                'use_instagram_field',
+                'use_nickname_field'
+            ]);
+        });
+    }
+};
