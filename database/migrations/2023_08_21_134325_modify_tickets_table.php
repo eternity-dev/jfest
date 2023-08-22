68173f5c89199ae7b00fe33422349b98489fa403
eternalbeginner <eternal.beginner.dev@gmail.com>
2023-08-22 11:40:26 +0800

    refactor: comments

--- database/migrations/2023_08_21_134325_modify_tickets_table.php
+++ database/migrations/2023_08_21_134325_modify_tickets_table.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::table('tickets', function (Blueprint $table) {
@@ -13,6 +16,9 @@ return new class extends Migration
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::table('tickets', function (Blueprint $table) {
